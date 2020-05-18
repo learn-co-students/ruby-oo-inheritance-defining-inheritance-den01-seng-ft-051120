3fa5076325f2947ca249ec1ab30573e5ec3ab4c9
github username <github email address>
2020-05-18 12:52:55 -0600

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,18 @@
-class Vehicle
+# frozen_string_literal: true
 
+class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    'vrrrrrrrooom!'
+  end
+
+  def fill_up_tank
+    'filling up!'
+  end
 end
